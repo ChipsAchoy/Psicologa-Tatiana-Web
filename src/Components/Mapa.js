9154653dac7694f940b158324d4208c182b46a18
ChipsAchoy <49498818+ChipsAchoy@users.noreply.github.com>
2024-03-01 17:53:03 -0600

    oh no causa

--- src/Components/Mapa.js
+++ src/Components/Mapa.js
@@ -1,5 +1,15 @@
 import React, { Component } from "react";
 import Fade from "react-reveal";
+import { withGoogleMap, GoogleMap, Marker } from 'react-google-maps';
+
+const Map = withGoogleMap(({ location }) => (
+  <GoogleMap defaultZoom={12} defaultCenter={location}>
+    <Marker position={location} />
+  </GoogleMap>
+));
+
+const [location, setLocation] = useState({ lat: 37.7749, lng: -122.4194 }); // Coordenadas de San Francisco por defecto
+
 
 class Mapa extends Component {
 
@@ -8,6 +18,11 @@ class Mapa extends Component {
     return(
       <Fade duration={1000}>
         <section id="mapa">
+        <Map
+        containerElement={<div style={{ height: '100%' }} />}
+        mapElement={<div style={{ height: '100%' }} />}
+        location={location}
+      />
         mI CARA CUANDO
         </section>
         
