39a1693c97f6cd89d5dd024022921fa2c848589b
ChipsAchoy <49498818+ChipsAchoy@users.noreply.github.com>
2024-03-01 23:05:36 -0600

    Correccion blogs

--- src/Components/Blogs.js
+++ src/Components/Blogs.js
@@ -1,50 +1,53 @@
-import React, { useEffect, useState } from 'react';
+import React, { Component } from 'react';
 import fs from 'fs';
 import { promisify } from 'util';
 
 const readdir = promisify(fs.readdir);
 const readFile = promisify(fs.readFile);
 
-const TextFilesReader = () => {
-  const [files, setFiles] = useState([]);
-
-  useEffect(() => {
-    const readFiles = async () => {
-      try {
-        // Ruta de la carpeta que contiene los archivos txt
-        const folderPath = 'blogs';
-        
-        // Lee la lista de archivos en la carpeta
-        const fileNames = await readdir(folderPath);
-
-        // Lee cada archivo y obtiene su contenido
-        const fileContents = await Promise.all(
-          fileNames.map(async (fileName) => {
-            const filePath = `${folderPath}/${fileName}`;
-            const content = await readFile(filePath, 'utf-8');
-            return { fileName, content };
-          })
-        );
-
-        setFiles(fileContents);
-      } catch (error) {
-        console.error('Error al leer archivos:', error);
-      }
+class Blogs extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      blogs: [],
     };
+  }
 
-    readFiles();
-  }, []);
+  componentDidMount() {
+    this.readBlogs();
+  }
 
-  return (
-    <div>
-      {files.map(({ fileName, content }) => (
-        <div key={fileName}>
-          <h1>{content.split('\n')[0]}</h1>
-          <p>{content.split('\n')[1]}</p>
-        </div>
-      ))}
-    </div>
-  );
-};
+  readBlogs = async () => {
+    try {
+      const folderPath = '/ruta/a/tu/carpeta';
+      const fileNames = await readdir(folderPath);
 
-export default TextFilesReader;
+      const blogContents = await Promise.all(
+        fileNames.map(async (fileName) => {
+          const filePath = `${folderPath}/${fileName}`;
+          const content = await readFile(filePath, 'utf-8');
+          return { fileName, content };
+        })
+      );
+
+      this.setState({ blogs: blogContents });
+    } catch (error) {
+      console.error('Error al leer archivos:', error);
+    }
+  };
+
+  render() {
+    return (
+      <div>
+        {this.state.blogs.map(({ fileName, content }) => (
+          <div key={fileName}>
+            <h1>{content.split('\n')[0]}</h1>
+            <p>{content.split('\n')[1]}</p>
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
+
+export default Blogs;
