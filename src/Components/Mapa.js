f388f47c14e7f4398397a64b4e9194d1e1293f97
ChipsAchoy <49498818+ChipsAchoy@users.noreply.github.com>
2024-03-01 22:35:46 -0600

    oh si viejo

--- src/Components/Mapa.js
+++ src/Components/Mapa.js
@@ -1,20 +1,40 @@
 import React, { Component } from "react";
-import Fade from "react-reveal";
+import Fade, { Slide } from "react-reveal";
 
-//const [location, setLocation] = useState({ lat: 37.7749, lng: -122.4194 }); // Coordenadas de San Francisco por defecto
 
 
 class Mapa extends Component {
 
 
   render() {
-    return(
-      <Fade duration={1000}>
+    const mapapic = "images/mapalugar.png";
+    return (
+      <Slide duration={2000}>
         <section id="mapa">
-        mI CARA CUANDO
+          <div className="row">
+            <h1 className="center">
+              Mapa
+            </h1>
+          </div>
+          <div className="row">
+            <div className="contenedor">
+              <img src={mapapic} alt="Mapa" />
+            </div>
+          </div>
+          <div className="row">
+            <Fade bottom duration={2000}>
+            <div className="contenedor">
+              <ul className="social">
+                <a href={"https://www.google.com/maps/place/Momentum+Lindora/@9.9533621,-84.195103,15z/data=!4m2!3m1!1s0x0:0x173dda3ec4762e7e?sa=X&ved=1t:2428&ictx=111"} className="button btn project-btn">
+                  <i className="fa fa-map-marker"></i>¿Cómo llegar?
+                </a>
+              </ul>
+              </div>
+            </Fade>
+          </div>
         </section>
-        
-      </Fade>
+
+      </Slide>
     );
   }
 }
--- src/Components/Servicios.js
+++ src/Components/Servicios.js
@@ -12,8 +12,8 @@ class Servicios extends Component {
           <div className="row">
             <div className="container">
               <div className="column">
-                <h2>¿CÓMO PUEDO AYUDARTE?</h2>
-                <h3>Psicoterapia</h3>
+                <h1>¿CÓMO PUEDO AYUDARTE?</h1>
+                <h2>PSICOTERAPIA</h2>
 
                 <p class="justified">{"La psicoterapia es un proceso colaborativo y orientado a objetivos en el que trabajare contigo para explorar tus pensamientos, emociones y comportamientos, así como para identificar y abordar los desafíos que puedas estar enfrentando. A través de conversaciones confidenciales y centradas en tus necesidades, la psicoterapia te ofrece un espacio seguro para reflexionar sobre tus experiencias pasadas, comprender patrones de pensamiento y comportamiento, y desarrollar estrategias efectivas para enfrentar tus preocupaciones y alcanzar tus metas personales. Ya sea que estés lidiando con el estrés, la ansiedad, la depresión, las relaciones interpersonales o cualquier otro aspecto de tu vida, la psicoterapia te proporciona herramientas y apoyo para promover el crecimiento personal y el bienestar emocional. Juntos, podemos trabajar para cultivar una mayor autoconciencia, mejorar tus habilidades de afrontamiento y fortalecer tu resiliencia para que puedas vivir una vida más plena y satisfactoria."}</p>
 
