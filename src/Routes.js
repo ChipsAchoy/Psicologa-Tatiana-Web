217e7557d5eb18e2c5e630abc8c406bb606991fe
David De la Hoz <divad0907@gmail.com>
2024-03-06 00:58:53 -0600

    fire base deployment

--- src/Routes.js
+++ src/Routes.js
@@ -1,10 +1,12 @@
 import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import Home from "./Components/Home";
 import Blogs from "./Components/Blogs";
 
 export const Routes = ()    => {
 
     return (
+        
         <Router>
             <Switch>
                 <Route path="/" exact component={Home} />
